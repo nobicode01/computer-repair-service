4720efdce55fa051fdd1a5324f8a26f8f12c2cf8
asigitj <asigitj1@gmail.com>
2022-12-16 09:26:29 +0700

    rapihin kode

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 const navbar = document.getElementsByTagName('nav')[0];
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     console.log(this.window.scrollY);
-    if(this.window.scrollY > 1){
+    if (this.window.scrollY > 1) {
         navbar.classList.replace('bg-transparent', 'nav-color');
-    } else  if (this.window.scrollY <= 0){
+    } else if (this.window.scrollY <= 0) {
         navbar.classList.replace('nav-color', 'bg-transparent');
     }
 });
 
-function pindahSection(url)
-{
-window.location = url;
+function pindahSection(url) {
+    window.location = url;
 }
